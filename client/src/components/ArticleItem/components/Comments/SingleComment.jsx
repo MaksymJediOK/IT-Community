277acbd1004740b222a0110cd86e8e74f998daa9
MaksymJediOK <73941252+MaksymJediOK@users.noreply.github.com>
2023-02-02 18:42:36 +0200

    select items, waiting for endpoints

--- client/src/components/ArticleItem/components/Comments/SingleComment.jsx
+++ client/src/components/ArticleItem/components/Comments/SingleComment.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import styles from './Coments.module.scss'
 import { Avatar, Box } from '@mui/material'
+import { CorrectDate } from 'utils/CorrectDate'
 
-export const SingleComment = () => {
+export const SingleComment = (props) => {
+	const { body, date, userName } = props
+	const validDate = CorrectDate(date)
+
 	return (
 		<div className={styles.comment}>
 			<Avatar />
 			<Box sx={{ marginLeft: '12px' }}>
 				<Box sx={{ display: 'flex' }}>
-					<div className={styles.comment_name}>Name surname</div>
-					<span className={styles.comment_date}>Sep 12, 2022</span>
+					<div className={styles.comment_name}>{userName}</div>
+					<span className={styles.comment_date}>{validDate}</span>
 				</Box>
-				<div className={styles.comment_content}>
-					This is a paragraph with more information about something important. This something
-					has many uses and is made of 100% recycled material.
-				</div>
+				<div className={styles.comment_content}>{body}</div>
 				<div className={styles.comment_action}>
 					<div className={styles.comment_action_text}>Reply</div>
 					<div className={styles.comment_action_text}>Like</div>
