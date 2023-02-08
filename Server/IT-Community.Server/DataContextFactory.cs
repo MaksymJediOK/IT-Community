c14152d2f8eac9606afe937085cffdfaae81f672
Vasyl Tyshchuk <vasyl.tyshchuk.xyz@gmail.com>
2023-02-08 18:38:14 +0200

    Fix entity framework

---
+++ Server/IT-Community.Server/DataContextFactory.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IT_Community.Server.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace IT_Community.Server
+{
+    public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        public DataContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets<Program>()
+                .AddEnvironmentVariables()
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<DataContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("ITCommunityConnection"));
+            return new DataContext(builder.Options);
+        }
+    }
+}
